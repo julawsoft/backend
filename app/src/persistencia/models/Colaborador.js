cf237d56fa9acae9839ce04a58f722b655c60ae7
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-10-06 19:00:37 +0100

    colaborador update implementation

--- app/src/persistencia/models/Colaborador.js
+++ app/src/persistencia/models/Colaborador.js
@@ -103,6 +103,51 @@ async function create(
   )
 }
 
+/**
+ * @param {string} nomeCompleto - Description
+ * @param {string} nomeProfissional - Description
+ * @param {string} dataNascimento - Description
+ * @param {string} uuid - Description
+ * @param {string} status - Description
+ * @param {string} funcao - Description
+ * @param {string} tipoColaboradorId - Description
+ * @param {string} inicial - Description
+ * @returns {number} id
+ * @returns {Object} Colaborador
+ */
+async function update(
+  {
+    nomeCompleto, 
+    nomeProfissional, 
+    dataNascimento,
+    funcao,
+    tipoColaboradorId,
+    uuid,
+    status,
+    inicial,
+    id
+  }
+) {
+
+  return Colaborador.update(
+    {
+      "nome_completo": nomeCompleto, 
+      "nome_profissional": nomeProfissional, 
+      "data_nascimento" : dataNascimento,
+      "uuid" : uuid,
+      "status" : status,
+      "funcao": funcao,
+      "tipo_colaborador_id": tipoColaboradorId,
+      "inicial": inicial,
+    },
+    {
+      where: {
+        id
+      }
+    }
+  )
+}
+
 
 /**
  * @returns {string} chave
@@ -116,7 +161,6 @@ async function getAllByKeyValue(chave, valor) {
   })
 }
 
-
 async function getAll() {
   return await Colaborador.findAll()
 }
@@ -127,4 +171,5 @@ module.exports = {
   create,
   getAllByKeyValue,
   getAll,
+  update
 };
