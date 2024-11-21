56867c88ce70d27bf4f798a0575fd743fe33d90d
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-11-21 01:42:21 +0100

    added new migration for tarefas

--- app/migrations/20241121003504-processo_tarefas.js
+++ app/migrations/20241121003504-processo_tarefas.js
@@ -3,30 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
-
-
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('processo_tarefas', {
       id: {
         allowNull: false,
