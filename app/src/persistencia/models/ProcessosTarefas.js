8f1e9ae5cb4e482de3867e286491577d34c0c62c
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-11-27 01:28:00 +0100

    adjustments to the fetch tarefas query to filter the status

--- app/src/persistencia/models/ProcessosTarefas.js
+++ app/src/persistencia/models/ProcessosTarefas.js
@@ -129,21 +129,23 @@ async function getTarefaById(id) {
 
 }
 
-async function getTarefaByColaboradorId(id) {
+async function getTarefaByColaboradorId(id, status = '0') {
 
   return ProcessosTarefas.sequelize.query(
     `
-      SELECT pt.descricao, pt.data_para_realizacao, pt.processo_id, p.assunto, p.gestor_id, cl.id
+      SELECT pt.descricao, pt.data_para_realizacao, pt.processo_id, p.assunto, p.gestor_id, cl.id, 
+      DATEDIFF(pt.data_para_realizacao, CURDATE()) as dias_em_falta
       FROM
-          processo_tarefas as pt
-          INNER JOIN processos as p ON pt.processo_id = p.id
-          INNER JOIN processo_equipa as pe ON p.id = pe.processo_id
-          INNER JOIN colaboradores as cl ON pe.colaborador_id = cl.id
+        processo_tarefas as pt
+        INNER JOIN processos as p ON pt.processo_id = p.id
+        INNER JOIN processo_equipa as pe ON p.id = pe.processo_id
+        INNER JOIN colaboradores as cl ON pe.colaborador_id = cl.id
       WHERE
-          cl.id = ?
+        pt.status = ? AND
+        cl.id = ?
     `,
     {
-      replacements: [id],
+      replacements: [status, id],
       type: QueryTypes.SELECT
     }
   );
